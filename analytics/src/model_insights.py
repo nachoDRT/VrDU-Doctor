8ac5dd64a392745e19edcc6d9d17e3a468539609
nachoDRT <iderodrigo@comillas.edu>
2024-06-05 13:42:50 +0200

    Configure Docker container for VSCode remote debugging

--- analytics/src/model_insights.py
+++ analytics/src/model_insights.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import debugpy
 from pathlib import Path
 from transformers import LayoutLMv2ForTokenClassification
 
@@ -7,7 +8,7 @@ from transformers import LayoutLMv2ForTokenClassification
 def access_weights(model):
     for name, param in model.named_parameters():
         msg = f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n"
-        print(msg)
+        breakpoint()
 
 
 if __name__ == "__main__":
@@ -16,6 +17,12 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", type=str)
     args = parser.parse_args()
 
+    # Connect remote debugger
+    debugpy.listen(("0.0.0.0", 5678))
+    print("Waiting for debugger to connect...")
+    debugpy.wait_for_client()
+
     model_path = os.path.join("/app/models", args.model_folder, args.model_name)
     model = LayoutLMv2ForTokenClassification.from_pretrained(model_path)
+    print("hola")
     access_weights(model)
