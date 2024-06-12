e4c8e395b6599f2e1a2d2b115e3b5a653fb369db
nachoDRT <iderodrigo@comillas.edu>
2024-06-12 11:49:17 +0200

    Control debug launch with argument

--- analytics/src/start.sh
+++ analytics/src/start.sh
@@ -2,4 +2,5 @@ chmod +x src/model_insights.py
 
 python src/model_insights.py \
     --model_folder "" \
-    --model_name "checkpoint-step_100-session_consistency_britanico"
+    --model_name "checkpoint-step_100-session_consistency_britanico" \
+    --debug True
