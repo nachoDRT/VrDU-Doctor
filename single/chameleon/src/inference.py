09cbcb7f0a0bc50dbdca8d958ab42d59c2354c86
nachoDRT <iderodrigo@comillas.edu>
2024-08-28 15:35:30 +0200

    Process Samples from HF Dataset

--- single/chameleon/src/inference.py
+++ single/chameleon/src/inference.py
@@ -1,26 +1,30 @@
-import torch
-import requests
 import os
+import torch
 import logging
-from huggingface_hub import login
+import requests
 from PIL import Image
+from datasets import load_dataset
+from huggingface_hub import login
 from transformers import ChameleonProcessor, ChameleonForConditionalGeneration
+
 
 CHAMELEON_DIR = os.path.join(os.getcwd(), "chameleon_files")
 
 
 def load_chameleon():
+
     chameleon_processor = ChameleonProcessor.from_pretrained(
         os.path.join(CHAMELEON_DIR, "processor")
     )
-    model = ChameleonForConditionalGeneration.from_pretrained(
+    chameleon_model = ChameleonForConditionalGeneration.from_pretrained(
         os.path.join(CHAMELEON_DIR, "model")
     )
 
-    return model, chameleon_processor
+    return chameleon_model, chameleon_processor
 
 
 def download_chameleon():
+
     if not os.path.exists(CHAMELEON_DIR):
         os.makedirs(CHAMELEON_DIR)
         logging.info(f"New directory created: {CHAMELEON_DIR}")
@@ -28,19 +32,76 @@ def download_chameleon():
         logging.info(f"{CHAMELEON_DIR} already exists")
 
     logging.info("Downloading Chameleon Processor")
-    processor = ChameleonProcessor.from_pretrained(
+    chameleon_processor = ChameleonProcessor.from_pretrained(
         "facebook/chameleon-7b", cache_dir=os.path.join(CHAMELEON_DIR, "processor")
     )
 
     logging.info("Downloading Chameleon Model")
-    model = ChameleonForConditionalGeneration.from_pretrained(
+    chameleon_model = ChameleonForConditionalGeneration.from_pretrained(
         "facebook/chameleon-7b",
         torch_dtype=torch.bfloat16,
         device_map="cuda:0",
         cache_dir=os.path.join(CHAMELEON_DIR, "model"),
     )
 
-    return model, processor
+    return chameleon_model, chameleon_processor
+
+
+def get_chameleon():
+
+    try:
+        logging.info("Loading downloaded files")
+        chameleon_model, chameleon_processor = load_chameleon()
+    except OSError:
+        logging.info("No downloaded files found: downloading files")
+        chameleon_model, chameleon_processor = download_chameleon()
+
+    return chameleon_model, chameleon_processor
+
+
+def get_test_dataset(dataset_name, subset, split="test"):
+    dataset = load_dataset(dataset_name, name=subset, split=split, num_proc=8)
+
+    return dataset
+
+
+def extract_grades(chameleon_model, chameleon_processor, dataset):
+
+    grades = []
+    prompt = """
+    Extract all subjects and their corresponding grades from the document in the image below. 
+    Each subject and grade pair should be identified and listed. 
+    For clarity, the output should be formatted similarly to this example:
+    {
+    "year_9": [
+        {"subject": "Greek", "grade": "50"},
+        {"subject": "Religious Studies", "grade": "89"},
+        {"subject": "Chinese", "grade": "51"},
+        {"subject": "Calculus II", "grade": "90"},
+        {"subject": "Biology and Geology", "grade": "59"},
+        {"subject": "World Sciences", "grade": "69"},
+        {"subject": "Business Economics", "grade": "50"},
+        {"subject": "Artistic Drawing", "grade": "55"}
+    ]
+    }
+    <image>
+    """
+
+    for sample in dataset:
+        image = sample["image"]
+
+        if not isinstance(image, Image.Image):
+            image = Image.fromarray(image)
+
+        inputs = chameleon_processor(prompt, image, return_tensors="pt").to(
+            model.device
+        )
+        inputs = {k: v.to(torch.float16) for k, v in inputs.items()}
+
+        output = chameleon_model.generate(**inputs, max_new_tokens=1024)
+        grades.append(output)
+
+        break
 
 
 if __name__ == "__main__":
@@ -48,21 +109,10 @@ if __name__ == "__main__":
     login(token=os.getenv("HUGGINGFACE_HUB_TOKEN"))
     logging.basicConfig(level=logging.INFO)
 
-    try:
-        logging.info("Loading downloaded files")
-        model, processor = load_chameleon()
-    except OSError:
-        logging.info("No downloaded files found: downloading files")
-        model, processor = download_chameleon()
+    # Get model and samples
+    model, processor = get_chameleon()
+    test_set = get_test_dataset("de-Rodrigo/merit", "en-digital-seq")
 
-    # prepare image and text prompt
-    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-    image = Image.open(requests.get(url, stream=True).raw)
-    prompt = "What do you see in this image?<image>"
-
-    inputs = processor(prompt, image, return_tensors="pt").to(model.device)
-    inputs = {k: v.to(torch.float16) for k, v in inputs.items()}
-
-    # autoregressively complete prompt
-    output = model.generate(**inputs, max_new_tokens=50)
-    print(processor.decode(output[0], skip_special_tokens=True))
+    # Process samples
+    grades = extract_grades(model, processor, test_set)
+    print(grades)
