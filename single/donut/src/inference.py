1df510daa61fe0b5651976335990e0af7bed48bb
nachoDRT <iderodrigo@comillas.edu>
2024-09-02 18:36:04 +0200

    Compute Saliency Map
    Include a workaround to avoid generates()'s @torch.no_grad() decorator:  https://discuss.huggingface.co/t/how-to-output-loss-from-model-generate/16999/12
    
    Using this workaround we keep the computation graph to obtain saliency maps

--- single/donut/src/inference.py
+++ single/donut/src/inference.py
@@ -1,8 +1,14 @@
 import re
+import os
 import torch
 import logging
+import matplotlib.pyplot as plt
 from datasets import load_dataset
 from transformers import DonutProcessor, VisionEncoderDecoderModel
+
+
+SALIENCY = True
+SALIENCIES_ROOT = "/app/saliencies/files/"
 
 
 def log_info(msg: str):
@@ -31,10 +37,37 @@ def get_image():
     dataset = load_dataset("hf-internal-testing/example-documents")
     img = dataset["test"][2]["image"]
 
+    save_img(img, os.path.join(SALIENCIES_ROOT, "dataset_img.png"))
+
     return img
 
 
-def compute_output(donut_m, donut_p):
+def compute_saliency(outputs, pixels, token_index=0):
+
+    token_logits = torch.stack(outputs.scores, dim=1)
+    token_probs = torch.softmax(token_logits, dim=-1)
+
+    target_token_prob = token_probs[0, token_index, outputs.sequences[0, token_index]]
+
+    if pixels.grad is not None:
+        pixels.grad.zero_()
+
+    target_token_prob.backward(retain_graph=True)
+
+    saliency = pixels.grad.data.abs().squeeze().mean(dim=0).cpu().numpy()
+    save_img(saliency, os.path.join(SALIENCIES_ROOT, "saliency.png"))
+
+    return token_index
+
+
+def save_img(img, path):
+    plt.imshow(img, cmap="hot")
+    plt.axis("off")
+    plt.savefig(path)
+    plt.close()
+
+
+def compute_output(donut_m, donut_p, pixels):
     log_info("Computing Output")
 
     task_prompt = "<s_cord-v2>"
@@ -45,8 +78,12 @@ def compute_output(donut_m, donut_p):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     donut_m.to(device)
 
-    outputs = donut_m.generate(
-        pixel_values.to(device),
+    pixels = pixels.to(device)
+    pixels.requires_grad = True
+
+    outputs = donut_m.generate.__wrapped__(
+        model,
+        pixels,
         decoder_input_ids=decoder_input_ids.to(device),
         max_length=donut_m.decoder.config.max_position_embeddings,
         early_stopping=True,
@@ -58,6 +95,13 @@ def compute_output(donut_m, donut_p):
         return_dict_in_generate=True,
         output_scores=True,
     )
+
+    if SALIENCY:
+        token_index = compute_saliency(outputs, pixels, 11)
+
+    token_id = outputs.sequences[0][token_index].item()
+    token_text = donut_p.tokenizer.decode([token_id])
+    log_info(f"Considered sequence token: {token_text}")
 
     sequence = donut_p.batch_decode(outputs.sequences)[0]
     sequence = sequence.replace(donut_p.tokenizer.eos_token, "").replace(
@@ -83,5 +127,5 @@ if __name__ == "__main__":
     pixel_values = processor(image, return_tensors="pt").pixel_values
 
     # Get output
-    output = compute_output(model, processor)
+    output = compute_output(model, processor, pixel_values)
     print(output)
