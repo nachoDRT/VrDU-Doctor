8d31fcf43dfbd20cddc8937b9407e173c4b3c057
nachoDRT <iderodrigo@comillas.edu>
2025-01-09 16:33:14 +0100

    Iterate over HF Dataset and Compute F1 (DONUT) Metric

--- saas/src/grades_extractor.py
+++ saas/src/grades_extractor.py
@@ -4,15 +4,25 @@ import os
 from os.path import join, abspath, dirname
 from openai import OpenAI
 from typing import Dict
+from donut import JSONParseEvaluator
+import numpy as np
+from datasets import load_dataset
+from io import BytesIO
+
+SAMPLES_LIMIT = 100
 
 
-def encode_image():
-    image_path = join(dirname(dirname(abspath(__file__))), "data", "test.jpg")
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
+def encode_image(img):
+
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    img_bytes = buffered.getvalue()
+
+    return base64.b64encode(img_bytes).decode("utf-8")
 
 
 def load_secrets(file_path: str) -> Dict:
+
     with open(file_path, encoding="utf-8") as config_file:
         secrets = json.load(config_file)
 
@@ -20,6 +30,7 @@ def load_secrets(file_path: str) -> Dict:
 
 
 def init_apis():
+
     secrets_path = join(dirname(dirname(abspath(__file__))), "config", "secrets.json")
     secrets = load_secrets(secrets_path)
     os.environ["OPENAI_API_KEY"] = secrets["openai"]
@@ -36,6 +47,7 @@ def detect_json(response: str) -> str:
 
 
 def clean_json(grades: str) -> Dict:
+
     raw_json = grades.encode("utf-8").decode("unicode_escape")
     corrected_json = raw_json.encode("latin-1").decode("utf-8")
     grades_dict = json.loads(corrected_json)
@@ -43,14 +55,7 @@ def clean_json(grades: str) -> Dict:
     return grades_dict
 
 
-def main():
-
-    init_apis()
-
-    # Getting the base64 string
-    base64_image = encode_image()
-
-    client = OpenAI()
+def get_ouput_seq(base64_image, client):
 
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -61,8 +66,18 @@ def main():
                     {
                         "type": "text",
                         "text": (
-                            "What is in this image? Please extract the academic subjects and their "
-                            "associated grades, and return the results in JSON format."
+                            "Look at the image and extract:\n"
+                            "- The subjects and their grades.\n"
+                            "- The level (9, 10, 11, or 12) they correspond to.\n\n"
+                            "You must return a SINGLE JSON object in the exact following format:\n\n"
+                            "{\n"
+                            '  "year_9": [  # Or year_10, year_11, or year_12 as appropriate\n'
+                            '    {"subject": "...", "grade": "..."},\n'
+                            '    {"subject": "...", "grade": "..."}\n'
+                            "  ]\n"
+                            "}\n\n"
+                            "DO NOT include any additional text, explanations, or comments. "
+                            "Use the key 'year_9', 'year_10', 'year_11', or 'year_12' based on what can be inferred from the image."
                         ),
                     },
                     {
@@ -76,7 +91,65 @@ def main():
 
     grades = detect_json(str(response.choices[0]))
     grades = clean_json(grades)
-    print(grades)
+
+    return grades
+
+
+def get_sample_data(sample):
+
+    print("Getting Image")
+
+    img = sample["image"]
+    gt = json.loads(sample["ground_truth"])
+    gt = gt["gt_parse"]
+
+    return img, gt
+
+
+def get_dataset_iterator():
+
+    print("Loading Dataset")
+
+    dataset = load_dataset("de-Rodrigo/merit", "en-digital-seq", split="train", streaming=True)
+    dataset_iterator = iter(dataset)
+
+    return dataset_iterator
+
+
+def process_dataset(dataset_iterator):
+
+    client = OpenAI()
+
+    evaluator = JSONParseEvaluator()
+    accs = []
+    output_list = []
+
+    for i, sample in enumerate(dataset_iterator):
+
+        image, gt = get_sample_data(sample)
+
+        base64_image = encode_image(image)
+
+        seq = get_ouput_seq(base64_image, client)
+        score = evaluator.cal_acc(seq, gt)
+
+        accs.append(score)
+        output_list.append(seq)
+        print(gt)
+        print(seq, score)
+        print("")
+
+        if i + 1 >= SAMPLES_LIMIT:
+            break
+
+    print(f"Mean accuracy: {np.mean(accs)}")
+
+
+def main():
+
+    init_apis()
+    dataset_iterator = get_dataset_iterator()
+    process_dataset(dataset_iterator)
 
 
 if __name__ == "__main__":
