70b3349902b95399802a9b22b6c4d020a6e3ea14
nachoDRT <iderodrigo@comillas.edu>
2025-01-13 18:44:49 +0100

    Obtain JSONL without Uploading Imgs to Repo
    They might be already uploaded and you just want to build your Jsonl again

--- saas/src/jsonl_gatherer.py
+++ saas/src/jsonl_gatherer.py
@@ -1,11 +1,12 @@
 from utils import *
 import requests
+import argparse
 
 
 TRAINING_SAMPLES = 10
 
 
-def upload_image_to_github(base64_image, image_name):
+def upload_image_to_github(base64_image, image_name, upload_img: bool = False):
 
     secrets_path = join(dirname(dirname(abspath(__file__))), "config", "secrets.json")
     secrets = load_secrets(secrets_path)
@@ -18,21 +19,25 @@ def upload_image_to_github(base64_image, image_name):
     remote_file_name = "saas/data"
 
     api_url = f"https://api.github.com/repos/{owner}/{repo}/contents/{remote_file_name}/{image_name}"
-    payload = {"message": "Uploading image", "content": base64_image, "branch": branch}
-    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
-    response = requests.put(api_url, json=payload, headers=headers)
 
-    if response.status_code == 201:
-        print("Image successfully uploaded")
-        print("Public URL:", response.json()["content"]["download_url"])
-    else:
-        print("Error uploading image:", response.status_code)
-        print("Details:", response.json())
+    if upload_img:
+        payload = {"message": "Uploading image", "content": base64_image, "branch": branch}
+        headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+        response = requests.put(api_url, json=payload, headers=headers)
+
+        if response.status_code == 201:
+            print("Image successfully uploaded")
+            print("Public URL:", response.json()["content"]["download_url"])
+        else:
+            print("Error uploading image:", response.status_code)
+            print("Details:", response.json())
+
+    return api_url
 
 
-def format_sample(image, image_name, gt):
+def format_sample(image, image_name, gt, args: dict):
     base64_image = encode_image(image)
-    url = upload_image_to_github(base64_image, image_name)
+    url = upload_image_to_github(base64_image, image_name, args["save_imgs"])
 
     sample = {
         "messages": [
@@ -73,14 +78,14 @@ def format_sample(image, image_name, gt):
     return sample
 
 
-def get_dataset_jsonl(decoded_ds_iterator, non_decoded_ds_iterator):
+def get_dataset_jsonl(decoded_ds_iterator, non_decoded_ds_iterator, args: dict):
 
     dataset_jsonl = []
 
     for i, (decoded_sample, non_decoded_sample) in enumerate(zip(decoded_ds_iterator, non_decoded_ds_iterator)):
         image, d_gt = get_sample_data(decoded_sample)
         image_name = get_sample_img_name(non_decoded_sample)
-        sample = format_sample(image, image_name, d_gt)
+        sample = format_sample(image, image_name, d_gt, args)
         dataset_jsonl.append(sample)
 
         if i + 1 >= TRAINING_SAMPLES:
@@ -89,13 +94,23 @@ def get_dataset_jsonl(decoded_ds_iterator, non_decoded_ds_iterator):
     return dataset_jsonl
 
 
-def main():
+def main(args: dict):
 
     decoded_ds_iterator = get_dataset_iterator()
     non_decoded_ds_iterator = get_dataset_iterator(True)
-    dataset_jsonl = get_dataset_jsonl(decoded_ds_iterator, non_decoded_ds_iterator)
+    dataset_jsonl = get_dataset_jsonl(decoded_ds_iterator, non_decoded_ds_iterator, args)
     save_dataset_jsonl("openai_finetuning_dataset.jsonl", dataset_jsonl)
 
 
 if __name__ == "__main__":
-    main()
+
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save_imgs", required=True, type=str)
+    args = parser.parse_args()
+
+    save_imgs = args.save_imgs.lower() in ("true", "1", "yes", "y")
+    args = {}
+    args["save_imgs"] = save_imgs
+
+    main(args)
