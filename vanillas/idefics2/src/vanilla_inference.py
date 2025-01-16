1a3ba8eccf3cca116749ecf3f8bc4087d2436a7b
nachoDRT <iderodrigo@comillas.edu>
2025-01-16 12:13:15 +0100

    Basic Vanilla Inference with Idefics2

---
+++ vanillas/idefics2/src/vanilla_inference.py
@@ -0,0 +1,122 @@
+import requests
+from PIL import Image
+from transformers import Idefics2Processor, Idefics2ForConditionalGeneration
+import torch
+from datasets import load_dataset
+import logging
+import json
+
+
+SAMPLES_LIMIT = 1
+
+
+def log_info(msg: str):
+    print("")
+    logging.info(msg)
+    print("")
+
+
+def get_dataset_iterator():
+    log_info("Loading Dataset")
+
+    dataset = load_dataset("de-Rodrigo/merit", "en-digital-seq", split="test", streaming=True)
+    dataset_iterator = iter(dataset)
+
+    return dataset_iterator
+
+
+def get_model_and_processor():
+    
+    model = Idefics2ForConditionalGeneration.from_pretrained("files/", device_map="auto")
+    model.to(device)
+
+    processor = Idefics2Processor.from_pretrained("HuggingFaceM4/idefics2-8b")
+
+    return model, processor
+
+
+def get_inputs(image):
+
+    images = [image]
+
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": (
+                        "Look at the image and extract:\n"
+                        "- The subjects and their grades.\n"
+                        "- The level (9, 10, 11, or 12) they correspond to.\n\n"
+                        "You must return a SINGLE JSON object in the exact following format:\n\n"
+                        "{\n"
+                        '  "year_9": [  # Or year_10, year_11, or year_12 as appropriate\n'
+                        '    {"subject": "...", "grade": "..."},\n'
+                        '    {"subject": "...", "grade": "..."}\n'
+                        "  ]\n"
+                        "}\n\n"
+                        "DO NOT include any additional text, explanations, or comments. "
+                        "Use the key 'year_9', 'year_10', 'year_11', or 'year_12' based on what can be inferred from the image."
+                    ),
+                },
+                {
+                    "type": "image",
+                },
+            ],
+        },
+    ],
+    
+    text = processor.apply_chat_template(messages, add_generation_prompt=True)
+    print(text)
+
+    inputs = processor(images=images, text=text, return_tensors="pt").to(device)
+
+    return inputs
+
+
+def get_answer(inputs):
+
+    generated_text = model.generate(**inputs, max_new_tokens=500)
+    generated_text = processor.batch_decode(generated_text, skip_special_tokens=True)[0]
+
+    return generated_text
+
+
+def get_sample_data(sample):
+
+    log_info("Getting Image")
+
+    img = sample["image"]
+
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+
+    gt = json.loads(sample["ground_truth"])
+    gt = gt["gt_parse"]
+
+    return img, gt
+
+
+def process_dataset(dataset_iterator):
+    
+    for i, sample in enumerate(dataset_iterator):
+        image, gt = get_sample_data(sample)
+
+        inputs = get_inputs(image)
+        generated_text = get_answer(inputs)
+        print("Generated text:")
+        print(generated_text)
+        print(gt)
+
+        if i + 1 >= SAMPLES_LIMIT:
+            break
+
+
+if __name__ == "__main__":
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model, processor = get_model_and_processor()
+    dataset_iterator = get_dataset_iterator()
+    process_dataset(dataset_iterator)
