077d4190267e99fd2632a138fb402641627c21d4
nachoDRT <iderodrigo@comillas.edu>
2025-02-12 10:04:08 +0100

    Save Processor Files

--- single/idefics2/src/train.py
+++ single/idefics2/src/train.py
@@ -236,6 +236,7 @@ class PushToHubCallback(Callback):
         # Save model locally
         epoch_subfolder = os.path.join(self.save_dir, f"{self.model_output_name}_{self.dataset_subset}_epoch{trainer.current_epoch}")
         pl_module.model.save_pretrained(epoch_subfolder)
+        pl_module.processor.save_pretrained(epoch_subfolder)
 
         # Upload model to the hub
         repo_id = f"{self.model_output_name}"
@@ -257,6 +258,7 @@ class PushToHubCallback(Callback):
         # Save model locally
         final_model_dir = os.path.join(self.save_dir, f"{self.model_output_name}_final")
         pl_module.model.save_pretrained(final_model_dir)
+        pl_module.processor.save_pretrained(final_model_dir)
 
         repo_id = f"{self.model_output_name}"
         
