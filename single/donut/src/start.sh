7d6f91ce9afbf7ee08f48df290658af6990b7dd8
nachoDRT <iderodrigo@comillas.edu>
2025-02-14 09:22:23 +0100

    Parse Arguments to Inference

--- single/donut/src/start.sh
+++ single/donut/src/start.sh
@@ -1,11 +1,19 @@
 chmod +x src/train.py
 chmod +x src/utils/inspect_hf_dataset.py
+chmod +x src/inference.py
 
 # python src/utils/inspect_hf_dataset.py
 
-python src/train.py \
-    --debug False \
-    --dataset_name de-Rodrigo/merit \
-    --dataset_subset 'en-digital-paragraph-degradation-seq'
+# python src/train.py \
+#     --debug False \
+#     --dataset_name de-Rodrigo/merit \
+#     --dataset_subset 'es-render-seq'
 
-# python src/inference.py
+python src/inference.py \
+    --dataset de-Rodrigo/merit-secret \
+    --subset all \
+    --model es-render-seq
+    # --model es-digital-paragraph-degradation-seq
+    # --model es-digital-line-degradation-seq
+    # --model es-digital-seq
+    # --model es-render-seq
