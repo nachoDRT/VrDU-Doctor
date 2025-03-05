0ee34a5faaadd311ac5326d3dd2513b8a17534da
nachoDRT <iderodrigo@comillas.edu>
2025-03-05 15:49:36 +0100

    Adjustments to Train with Real Samples

--- single/donut/src/start.sh
+++ single/donut/src/start.sh
@@ -4,15 +4,15 @@ chmod +x src/inference.py
 
 # python src/utils/inspect_hf_dataset.py
 
-# python src/train.py \
-#     --debug False \
-#     --dataset_name de-Rodrigo/merit \
-#     --dataset_subset 'es-render-seq'
+python src/train.py \
+    --debug False \
+    --dataset_name de-Rodrigo/merit-secret \
+    --dataset_subset real
 
-python src/inference.py \
-    --dataset de-Rodrigo/merit-secret \
-    --subset all \
-    --model es-render-seq
+# python src/inference.py \
+#     --dataset de-Rodrigo/merit-secret \
+#     --subset all \
+#     --model es-render-seq
     # --model es-digital-paragraph-degradation-seq
     # --model es-digital-line-degradation-seq
     # --model es-digital-seq
