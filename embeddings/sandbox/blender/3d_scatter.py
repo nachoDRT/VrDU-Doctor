97533903fe4c9e2bf4ec8636198868fc42c77157
nachoDRT <iderodrigo@comillas.edu>
2025-04-01 15:41:50 +0200

    Include Synthetic Samples in the Plot

--- embeddings/sandbox/blender/3d_scatter.py
+++ embeddings/sandbox/blender/3d_scatter.py
@@ -3,30 +3,53 @@ import pandas as pd
 
 
 FACTOR = 0.219938
-REAL_SAMPLES_BLUE = (0.069, 0.35, 1, 1)
+COLORS = [(0.069, 0.35, 1, 1), (0.475, 0, 1, 1)]
+SHEETS = [
+    "real",
+    "synthetic_es-digital-line-degradation-seq",
+    "synthetic_es-digital-paragraph-degradation-seq",
+    "synthetic_es-digital-seq",
+    "synthetic_es-digital-rotation-degradation-seq",
+    "synthetic_es-digital-zoom-degradation-seq",
+    "synthetic_es-render-seq",
+]
 
 
 def get_real_samples_material():
 
-    blue_mat = bpy.data.materials.get("BlueMaterial")
-    if blue_mat is None:
-        blue_mat = bpy.data.materials.new(name="BlueMaterial")
-        blue_mat.use_nodes = True
-        bsdf = blue_mat.node_tree.nodes.get("Principled BSDF")
-        if bsdf is not None:
-            bsdf.inputs["Base Color"].default_value = REAL_SAMPLES_BLUE
+    blue_mat = None  # bpy.data.materials.get("BlueMaterial")
+    green_mat = None  # bpy.data.materials.get("GreenMaterial")
 
-    return blue_mat
+    materials = [blue_mat, green_mat]
+
+    for i, material in enumerate(materials):
+
+        if material is None:
+            material = bpy.data.materials.new(name="BlueMaterial")
+            material.use_nodes = True
+            bsdf = material.node_tree.nodes.get("Principled BSDF")
+
+            if bsdf is not None:
+                bsdf.inputs["Base Color"].default_value = COLORS[i]
+
+            materials[i] = material
+
+    return materials
 
 
 def load_data():
     path = bpy.path.abspath("//plots/df_all_pca_donut.xlsx")
-    df = pd.read_excel(path)
 
-    return df
+    data = {}
+
+    for sheet_name in SHEETS:
+        df = pd.read_excel(path, sheet_name=sheet_name)
+        data[sheet_name] = df
+
+    return data
 
 
-def generate_scatter_plot(df, mat):
+def real_samples_scatter_plot(df, mat):
 
     collection_name = "real_samples"
     scatter_coll = bpy.data.collections.get(collection_name)
@@ -41,6 +64,7 @@ def generate_scatter_plot(df, mat):
 
         bpy.ops.mesh.primitive_uv_sphere_add(radius=0.015, location=(x, y, z))
         sphere = bpy.context.active_object
+        sphere.name = row["name"]
 
         if sphere.data.materials:
             sphere.data.materials[0] = mat
@@ -52,8 +76,14 @@ def generate_scatter_plot(df, mat):
             bpy.context.scene.collection.objects.unlink(sphere)
 
 
+def synth_samples_scatter_plot(df, mat):
+    pass
+
+
 if __name__ == "__main__":
 
-    real_samples_mat = get_real_samples_material()
-    df_data = load_data()
-    generate_scatter_plot(df_data, real_samples_mat)
+    materials = get_real_samples_material()
+
+    data = load_data()
+    real_samples_scatter_plot(data["real"], materials[0])
+    real_samples_scatter_plot(data["synthetic_es-digital-line-degradation-seq"], materials[1])
