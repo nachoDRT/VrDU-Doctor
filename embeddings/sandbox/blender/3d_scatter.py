5b2ce4565dcf70b02df5888996169910710fedd5
nachoDRT <iderodrigo@comillas.edu>
2025-04-01 13:02:36 +0200

    Include Spheres in Collection

--- embeddings/sandbox/blender/3d_scatter.py
+++ embeddings/sandbox/blender/3d_scatter.py
@@ -27,6 +27,13 @@ def load_data():
 
 
 def generate_scatter_plot(df, mat):
+
+    collection_name = "real_samples"
+    scatter_coll = bpy.data.collections.get(collection_name)
+    if scatter_coll is None:
+        scatter_coll = bpy.data.collections.new(collection_name)
+        bpy.context.scene.collection.children.link(scatter_coll)
+
     for index, row in df.iterrows():
         x = row["PC1"] * FACTOR
         y = row["PC2"] * FACTOR
@@ -39,6 +46,10 @@ def generate_scatter_plot(df, mat):
             sphere.data.materials[0] = mat
         else:
             sphere.data.materials.append(mat)
+
+        scatter_coll.objects.link(sphere)
+        if sphere.name in bpy.context.scene.collection.objects:
+            bpy.context.scene.collection.objects.unlink(sphere)
 
 
 if __name__ == "__main__":
